17e0bd9a58fef971ac7ae452c75eebc069ef73d0
Ilia Meerovich <imeerovich@habana.ai>
2019-07-15 17:28:27 +0300

    Master promotion to v0.3.0
    Change-Id: I0dbb50f3a5f6e4ce36e95271f1002d695f82a744
    Signed-off-by: Ilia Meerovich <imeerovich@habana.ai>

--- include/specs/version.h
+++ include/specs/version.h
@@ -8,9 +8,9 @@
 #ifndef HL_VERSION_H
 #define HL_VERSION_H
 
-#define HL_DRIVER_DATE		"20190602"
+#define HL_DRIVER_DATE		"20190715"
 #define HL_DRIVER_MAJOR		0
-#define HL_DRIVER_MINOR		2
+#define HL_DRIVER_MINOR		3
 #define HL_DRIVER_PATCHLEVEL	0
 
 #endif /* HL_VERSION_H */
