6a8e0dddc58c60e4e6787dc11201ef3226173fb7
Itay Shay <ishay@habana.ai>
2020-04-30 13:19:18 +0300

    [SW-0] Promote master to 0.9.0
    Change-Id: If7bf5f97f72d4917ea44cc290441bf6f54ee9144
    Signed-off-by: Itay Shay <ishay@habana.ai>

--- include/specs/version.h
+++ include/specs/version.h
@@ -1,3 +1,4 @@
+
 /* SPDX-License-Identifier: MIT
  *
  * Copyright 2016-2019 HabanaLabs, Ltd.
@@ -8,9 +9,9 @@
 #ifndef HL_VERSION_H
 #define HL_VERSION_H
 
-#define HL_DRIVER_DATE		"20200305"
-#define HL_DRIVER_MAJOR		0
-#define HL_DRIVER_MINOR		8
-#define HL_DRIVER_PATCHLEVEL	0
+#define HL_DRIVER_DATE          "20200430"
+#define HL_DRIVER_MAJOR         0
+#define HL_DRIVER_MINOR         9
+#define HL_DRIVER_PATCHLEVEL    0
 
 #endif /* HL_VERSION_H */
