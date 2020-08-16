176771037b15a5627503c3dce0aa61202e96da0a
Alexey Nikandrov <anikandrov@habana.ai>
2020-08-16 09:29:02 +0300

    [SW-0] Promote master to 0.11.0
    Change-Id: I8dd25415817f905def9ac4a0f7e4d52293c053a2
    Signed-off-by: Alexey Nikandrov <anikandrov@habana.ai>

--- include/specs/version.h
+++ include/specs/version.h
@@ -8,9 +8,9 @@
 #ifndef HL_VERSION_H
 #define HL_VERSION_H
 
-#define HL_DRIVER_DATE          "20200624"
+#define HL_DRIVER_DATE          "20200816"
 #define HL_DRIVER_MAJOR         0
-#define HL_DRIVER_MINOR         10
+#define HL_DRIVER_MINOR         11
 #define HL_DRIVER_PATCHLEVEL    0
 
 #endif /* HL_VERSION_H */
