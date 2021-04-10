98ab46e189af8a9f121a2827ff500570a55eaa79
Oded Gabbay <ogabbay@habana.ai>
2021-04-10 22:23:42 +0300

    [SW-33623] tests: enhance dmabuf test
    Change-Id: I2930e6339a94fe7d8ece34d795678223891b61bd
    Signed-off-by: Oded Gabbay <ogabbay@habana.ai>

---
+++ include/specs/common/importer_drv.h
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: MIT
+ *
+ * Copyright 2021 HabanaLabs, Ltd.
+ * All Rights Reserved.
+ *
+ */
+
+#ifndef IMPORTER_DRV_H
+#define IMPORTER_DRV_H
+
+#include <linux/types.h>
+#include <linux/ioctl.h>
+
+struct hl_importer_reg_dmabuf_mr_in {
+	__u64 offset;
+	__u64 length;
+	__u64 iova;
+	__u32 fd;
+	__u32 access_flags;
+	__u64 res[4];
+};
+
+struct hl_importer_reg_dmabuf_mr_out {
+	/* Handle of MR */
+	__u64 mr_handle;
+	__u64 res[7];
+};
+
+union hl_importer_reg_dmabuf_mr_args {
+	struct hl_importer_reg_dmabuf_mr_in in;
+	struct hl_importer_reg_dmabuf_mr_out out;
+};
+
+struct hl_importer_dereg_mr_args {
+	/* Handle of MR */
+	__u64 mr_handle;
+	__u64 res[3];
+};
+
+/* Register the DMABUF and return an "MR" that is associated with it */
+#define HL_IMPORTER_IOCTL_REG_DMABUF_MR \
+			_IOWR('I', 0x01, union hl_importer_reg_dmabuf_mr_args)
+
+#define HL_IMPORTER_IOCTL_DEREG_MR \
+			_IOWR('I', 0x02, struct hl_importer_dereg_mr_args)
+
+#define HL_IMPORTER_COMMAND_START	0x01
+#define HL_IMPORTER_COMMAND_END		0x03
+
+#endif /* IMPORTER_DRV_H */
