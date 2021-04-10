6140c9507def39f6aca10acd7c55f380aa0a56e4
Oded Gabbay <ogabbay@habana.ai>
2021-04-10 22:23:42 +0300

    [SW-33623] tests: enhance dmabuf test
    Change-Id: I2930e6339a94fe7d8ece34d795678223891b61bd
    Signed-off-by: Oded Gabbay <ogabbay@habana.ai>

--- tests/common/dma_buf.c
+++ tests/common/dma_buf.c
@@ -7,6 +7,7 @@
 
 #include "hlthunk_tests.h"
 #include "kvec.h"
+#include "specs/common/importer_drv.h"
 
 #include <stdarg.h>
 #include <stddef.h>
@@ -17,22 +18,68 @@
 #include <errno.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/ioctl.h>
+
+static int ibv_reg_dmabuf_mr(int fd, uint64_t offset, uint64_t length,
+				uint64_t iova, uint32_t dmabuf_fd,
+				uint32_t access_flags, uint64_t *mr_handle)
+{
+	union hl_importer_reg_dmabuf_mr_args args;
+	int rc;
+
+	if (!mr_handle)
+		return -EINVAL;
+
+	memset(&args, 0, sizeof(args));
+	args.in.offset = offset;
+	args.in.length = length;
+	args.in.iova = iova;
+	args.in.fd = dmabuf_fd;
+	args.in.access_flags = access_flags;
+
+	rc = ioctl(fd, HL_IMPORTER_IOCTL_REG_DMABUF_MR, &args);
+	if (rc)
+		return rc;
+
+	*mr_handle = args.out.mr_handle;
+
+	return 0;
+}
+
+static int ibv_dereg_mr(int fd, uint64_t mr_handle)
+{
+	struct hl_importer_dereg_mr_args args;
+
+	memset(&args, 0, sizeof(args));
+	args.mr_handle = mr_handle;
+
+	return ioctl(fd, HL_IMPORTER_IOCTL_DEREG_MR, &args);
+}
 
 void test_dmabuf(void **state)
 {
 	struct hltests_state *tests_state = (struct hltests_state *) *state;
+	int imp_fd = tests_state->imp_fd;
 	struct hlthunk_hw_ip_info hw_ip;
+	int fd = tests_state->fd;
+	uint64_t mr_handle = 0;
 	int rc;
 
 	if (hltests_is_pldm(tests_state->fd))
 		skip();
 
-	if (tests_state->imp_fd < 0) {
+	if (imp_fd < 0) {
 		printf("Skipping test because importer device is missing\n");
 		skip();
 	}
 
-	rc = hlthunk_get_hw_ip_info(tests_state->fd, &hw_ip);
+	rc = hlthunk_get_hw_ip_info(fd, &hw_ip);
+	assert_int_equal(rc, 0);
+
+	rc = ibv_reg_dmabuf_mr(imp_fd, 0, 0, 0, 0, 0, &mr_handle);
+	assert_int_equal(rc, 0);
+
+	rc = ibv_dereg_mr(imp_fd, mr_handle);
 	assert_int_equal(rc, 0);
 }
 
