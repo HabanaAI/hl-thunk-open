f82510a6afbc0fa54a014378c9dd13cd715decae
Alon Mizrahi <amizrahi@habana.ai>
2021-05-03 11:54:09 +0300

    [SW-0] tests: add verbose messages to dma_entire_dram test
    Change-Id: Iece83035c2e71b4cd6aa6493936429d4acd691d3
    Signed-off-by: Alon Mizrahi <amizrahi@habana.ai>

--- tests/common/hlthunk_tests.h
+++ tests/common/hlthunk_tests.h
@@ -465,6 +465,7 @@ const char *hltests_get_parser_pciaddr(void);
 const char *hltests_get_config_filename(void);
 int hltests_get_parser_run_disabled_tests(void);
 int hltests_get_verbose_enabled(void);
+uint32_t hltests_get_cur_seed(void);
 int hltests_is_legacy_mode_enabled(void);
 bool hltests_is_simulator(int fd);
 bool hltests_is_goya(int fd);
