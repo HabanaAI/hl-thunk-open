6ef5e55242add2e54bc6e6611356123bb8c5db4d
Ido Gan <igan@habana.ai>
2021-06-06 17:25:50 +0300

    [SW-0] Promote to 1.0.0
    Change-Id: I9bbdd146d84349dec1864b85a32255706eb94478

--- include/specs/common/version.h
+++ include/specs/common/version.h
@@ -8,9 +8,9 @@
 #ifndef HL_VERSION_H
 #define HL_VERSION_H
 
-#define HL_DRIVER_DATE          "20210518"
-#define HL_DRIVER_MAJOR         0
-#define HL_DRIVER_MINOR         16
+#define HL_DRIVER_DATE          "20210606"
+#define HL_DRIVER_MAJOR         1
+#define HL_DRIVER_MINOR         0
 #define HL_DRIVER_PATCHLEVEL    0
 
 #endif /* HL_VERSION_H */
