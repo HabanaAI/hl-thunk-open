f2b9fc75799e6ca1ba61c36f359fc741715e9694
Oded Gabbay <ogabbay@habana.ai>
2021-09-01 16:22:27 +0300

    Revert "[SW-33623] Add API to retrieve FD for dma-buf"
    This reverts commit 529d05ecebe6470d4b2732f179a841609e5f4cf8.
    
    Change-Id: I4c7c54a75f55a9e7cf7f2677809e74b33dba0455

--- src/libhlthunk.h
+++ src/libhlthunk.h
@@ -102,9 +102,7 @@ do { \
 	.fp_hlthunk_staged_cs_timeout = \
 		hlthunk_staged_command_submission_timeout_original,\
 	.fp_hlthunk_get_hw_block = hlthunk_get_hw_block_original,\
-	.fp_hlthunk_wait_for_interrupt = hlthunk_wait_for_interrupt,\
-	.fp_hlthunk_device_memory_export_dmabuf_fd =\
-		hlthunk_device_memory_export_dmabuf_fd\
+	.fp_hlthunk_wait_for_interrupt = hlthunk_wait_for_interrupt\
 }
 
 int hlthunk_command_submission_original(int fd, struct hlthunk_cs_in *in,
